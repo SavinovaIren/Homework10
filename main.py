7de8fb9170e01352a67866779df40aa86319cfb0
SavinovaIren <irka887@gmail.com>
2022-04-17 22:44:54 +0300

    all functions in page

--- main.py
+++ main.py
@@ -2,32 +2,41 @@ import json
 
 from flask import Flask
 
+with open("candidates.json", 'r', encoding='utf-8') as file:
+    candidates_list = json.load(file)
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-    with open("candidates.json", 'r', encoding='utf-8') as file:
-        candidates_list = json.load(file)
-    result = '<pre>'
+    result = '<pre>\n'
     for candidate in candidates_list:
-        result += {
-            f'Имя кандидата - {candidate["name"]}\n'
-            f'Позиция кандидата - {candidate["position"]}\n'
-            f'Навыки через запятую - {candidate["skills"]}\n'
-        }
-        result += '<pre>'
+        result += f'Имя кандидата - {candidate["name"]}\nПозиция кандидата - {candidate["position"]}\nНавыки через запятую - {candidate["skills"]}\n\n'
+    result += '<pre>'
     return result
 
 
-@app.route("/candidates/<id>")
+@app.route("/candidates/<int:id>")
 def candidate(id):
-    pass
+    for candidate in candidates_list:
+        if candidate["id"] == id:
+            result = f'<img src = {candidate["picture"]}\n\n>'
+            result += '<pre>\n'
+            result += f'Имя кандидата - {candidate["name"]}\nПозиция кандидата - {candidate["position"]}\nНавыки через запятую - {candidate["skills"]}\n\n'
+            result += '<pre>'
+            return result
 
 
 @app.route("/skills/<skill>")
-def skill(skills):
-    pass
+def skill(skill):
+    result = '<pre>\n'
+    for candidate in candidates_list:
+        list = candidate["skills"].lower().split(", ")
+        if skill in list:
+            result += f'Имя кандидата - {candidate["name"]}\nПозиция кандидата - {candidate["position"]}\nНавыки через запятую - {candidate["skills"]}\n\n'
+    result += '<pre>\n'
+    return result
 
 
 app.run()
